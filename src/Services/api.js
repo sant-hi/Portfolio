9c4b379db6d8bdcca8008447ac31ffac36816ddc
sant-hi <spsh103@gmail.com>
2026-08-14 19:15:33 +0530

    Portfolio React frontend

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = "https://portfolio-backend-422t.onrender.com/api";
 
 export const sendContactMessage = async (data) => {
     return await axios.post(
