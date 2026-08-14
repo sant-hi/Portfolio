521dcfe82bc0e3623ff6959492f917830dcfb761
sant-hi <spsh103@gmail.com>
2026-08-14 20:10:57 +0530

    Responsive banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import image from "../assets/Images/banner_new.png";
+import "./CSS/Banner.css";
 function Banner() {
   const name="Santhi Priya"
   const role="Python Full Stack Developer"
