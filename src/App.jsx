41e607bb2b034c1c65758d19348dd75c04a0fe68
sant-hi <spsh103@gmail.com>
2026-08-20 11:53:01 +0530

    Github Link added with image

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Contact from "./Components/Contact";
 import My_Footer from "./Components/My_Footer";
 
 // Project Images
-import traffic from "./assets/Images/traffic.jpeg";
+import portfolio from "./assets/Images/portfolio.avif";
 import job from "./assets/Images/job.jpeg";
 import ecommerce from "./assets/Images/ecommerce.jpeg";
 
